045d0572e9d10c61ccaa127e74df92caec4a53e1
M1r12on <marloncadell@gmx.de>
2025-01-25 05:34:02 +0100

    comments

--- eth-project/hardhat.config.js
+++ eth-project/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`], // Achte auf das "0x" vor deinem privaten Schlüssel
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Achte auf das "0x" vorm privaten Schlüssel
     },
   },
 };
