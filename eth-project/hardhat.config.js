7a4b521252c70b0d2c409ce517866eb4870b1c11
M1r12on <marloncadell@gmx.de>
2025-02-06 17:22:02 +0100

    update: config

--- eth-project/hardhat.config.js
+++ eth-project/hardhat.config.js
@@ -7,7 +7,12 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`], // Achte auf das "0x" vor deinem privaten Schlüssel
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Achte auf das "0x" vor dem privaten Schlüssel
+    },
+    arbitrumOne: { // Arbitrum One mit Infura
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`, // Infura RPC für Arbitrum
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Wallet Private Key (aus .env)
+      chainId: 42161, // Chain-ID für Arbitrum One Mainnet
     },
   },
 };
