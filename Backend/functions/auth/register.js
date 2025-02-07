9e3af96620a85e05fa9a3e6fdb9f8c5df4cd447a
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2025-02-07 08:02:23 +0100

    feat: update initial ETH amount to 0.01 for user registration

--- Backend/functions/auth/register.js
+++ Backend/functions/auth/register.js
@@ -7,7 +7,7 @@ const { ethers } = require('ethers');
 const prisma = new PrismaClient();
 
 
-const INITIAL_ETH_AMOUNT = ethers.parseEther('0.007');
+const INITIAL_ETH_AMOUNT = ethers.parseEther('0.01');
 
 console.log("INITIAL_ETH_AMOUNT:", INITIAL_ETH_AMOUNT);
 
